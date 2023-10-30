15933f5f4561e095eaade23718fa25b51e66158b
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-10-29 23:30:50 -0300

    Documentação com OpenApi usando @Operation

--- src/main/java/dev/samanda/controlecontatos/controller/ContatoController.java
+++ src/main/java/dev/samanda/controlecontatos/controller/ContatoController.java
@@ -1,7 +1,9 @@
 package dev.samanda.controlecontatos.controller;
 
 import dev.samanda.controlecontatos.model.Contato;
+import dev.samanda.controlecontatos.model.dtos.ContatoCriacaoDto;
 import dev.samanda.controlecontatos.service.ContatoService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,30 +20,35 @@ public class ContatoController {
         this.service = service;
     }
 
+    @Operation(summary = "Obter Contato por ID")
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Contato> findById(@PathVariable Long id){
         return ResponseEntity.status(200).body(service.findById(id));
     }
 
+    @Operation(summary = "Listar todos os Contatos")
     @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<Contato>> findAll(){
         return ResponseEntity.status(200).body(service.findAll());
     }
 
+    @Operation(summary = "Adicionar um novo Contato a uma Pessoa")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<Contato> create(@RequestBody Contato contato){
+    public ResponseEntity<Contato> create(@RequestBody ContatoCriacaoDto contato){
         return ResponseEntity.status(201).body(service.create(contato));
     }
 
+    @Operation(summary = "Atualizar Contato por ID")
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<Contato> update(@RequestBody Contato contato, @PathVariable Long id){
         return ResponseEntity.status(200).body(service.update(contato, id));
     }
 
+    @Operation(summary = "Deletar Contato por ID")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> delete(@PathVariable Long id){
--- src/main/java/dev/samanda/controlecontatos/service/ContatoService.java
+++ src/main/java/dev/samanda/controlecontatos/service/ContatoService.java
@@ -1,29 +1,47 @@
 package dev.samanda.controlecontatos.service;
 
 import dev.samanda.controlecontatos.model.Contato;
+import dev.samanda.controlecontatos.model.Pessoa;
+import dev.samanda.controlecontatos.model.dtos.ContatoCriacaoDto;
 import dev.samanda.controlecontatos.repository.ContatoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import dev.samanda.controlecontatos.repository.PessoaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class ContatoService {
 
-    @Autowired
-    private ContatoRepository repository;
+    private final ContatoRepository contatoRepository;
+
+    private final PessoaRepository pessoaRepository;
+
+    public ContatoService(ContatoRepository contatoRepository, PessoaRepository pessoaRepository) {
+        this.contatoRepository = contatoRepository;
+        this.pessoaRepository = pessoaRepository;
+    }
+
 
     public Contato findById(Long id){
-        return repository.findById(id).orElseThrow(() -> new NoSuchElementException());
+        return contatoRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
     }
 
     public List<Contato> findAll(){
-        return repository.findAll();
+        return contatoRepository.findAll();
     }
 
-    public Contato create(Contato contato){
-        return repository.save(contato);
+    public Contato create(ContatoCriacaoDto contatoDto){
+        Pessoa pessoa = pessoaRepository.findById(contatoDto.idPessoa()).orElseThrow(() ->
+                new IllegalArgumentException("A pessoa informada não existe. Por favor, cadastre-a antes do contato."));
+
+        Contato contato = new Contato();
+        contato.setTipoContato(contatoDto.tipoContato());
+        contato.setContato(contatoDto.contato());
+        contato.setPessoa(pessoa);
+
+        return contatoRepository.save(contato);
     }
 
     public Contato update(Contato contato, Long id){
@@ -31,10 +49,10 @@ public class ContatoService {
 
         contatoBD.setContato(contato.getContato());
 
-        return repository.save(contatoBD);
+        return contatoRepository.save(contatoBD);
     }
     public void delete(Long id){
-        repository.deleteById(id);
+        contatoRepository.deleteById(id);
     }
 
 }
